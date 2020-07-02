19a245780bdb0a8d0612e726a80a098bab68a62e
Dr. Olaf Filies <olaf.filies@t-online.de>
2020-07-03 00:41:32 +0200

    Add new libs to the pal hpp

--- src/framework/wiced-43xxx/pal/TLE94112-pal-wiced.hpp
+++ src/framework/wiced-43xxx/pal/TLE94112-pal-wiced.hpp
@@ -29,25 +29,11 @@
  */
 
 #include "../../../corelib/TLE94112.hpp"
+#include "gpio-wiced.hpp"
+#include "timer-wiced.hpp"
+#include "spic-wiced.hpp"
 #include <platform.h>
 #include <wiced_platform.h>
-
-/*!
- * Standard chip select pin for first TLE94112 shield
- */
-#define TLE94112_PIN_CS1     (WICED_GPIO_7)
-
-/*!
- * Standard chip select pin for second TLE94112 shield
- * To use a second shield with different CS pin you have
- * to remove the 0 Ohm resistor R7 and solder it to R8
- */
-#define TLE94112_PIN_CS2     (WICED_GPIO_17)
-
-/*!
- * Standard TLE94112 enable pin
- */
-#define TLE94112_PIN_EN      (WICED_GPIO_36)
 
 /**
  * @brief represents a motor connected to a TLE94112
