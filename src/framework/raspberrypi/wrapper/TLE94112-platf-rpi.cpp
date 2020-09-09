51b9b490eee4fe3761aedd7dff57af28faa1ed86
Julian Eder <Julian.Eder@infineon.com>
2020-09-09 18:05:13 +0200

    prepare rpi frwk file structure

---
+++ src/framework/raspberrypi/wrapper/TLE94112-platf-rpi.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file        TLE94112-platf-ino.cpp
+ * @brief       TLE94112-platf-ino.cpp Arduino Hardware Platforms
+ * @date        Mai 2020
+ * @version     2.0.0
+ * @copyright   Copyright (c) 2019-2020 Infineon Technologies AG
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "TLE94112-platf-ino.hpp"
+
+#if (TLE94112_FRAMEWORK == TLE94112_FRMWK_ARDUINO)
+
+
+#endif /** TLE94112_FRAMEWORK **/
---
+++ src/framework/raspberrypi/wrapper/TLE94112Motor-platf-rpi.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file        TLE94112Motor-platf-ino.hpp
+ * @brief       TLE94112Motor-platf-ino.hpp Arduino Hardware Platforms
+ * @date        Mai 2020
+ * @version     2.0.0
+ * @copyright   Copyright (c) 2019-2020 Infineon Technologies AG
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef TLE94112MOTOR_PLATF_INO_HPP_
+#define TLE94112MOTOR_PLATF_INO_HPP_
+
+#include "TLE94112-platf-ino.hpp"
+
+#if (TLE94112_FRAMEWORK == TLE94112_FRMWK_ARDUINO)
+
+#include "../../../corelib/TLE94112Motor.hpp"
+
+/**
+ * @addtogroup platfIno
+ * @{
+ */
+
+
+/**
+ * @}
+ */
+
+#endif /** TLE94112_FRAMEWORK **/
+#endif /** TLE94112MOTOR_PLATF_INO_HPP_ **/
