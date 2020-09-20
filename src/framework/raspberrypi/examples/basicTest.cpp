e7bf066aa088105f857c8ffe0b3e52330a71a414
Julian Eder <Julian.Eder@infineon.com>
2020-09-20 15:20:40 +0200

    seperate hpp and cpp, remove unfixed code fragments

--- src/framework/raspberrypi/examples/basicTest.cpp
+++ src/framework/raspberrypi/examples/basicTest.cpp
@@ -1,10 +1,4 @@
-#include <stdint.h>
-#include <stdio.h>
-#include "../../../config/tle94112-conf.hpp"
-#include <TLE94112-platf-rpi.hpp>
-#include <TLE94112-pal-rpi.hpp>
-#include <TLE94112.hpp>
-#include <TLE94112Motor.hpp>
+#include "basicTest.hpp"
 
 /**
  * Raspberry Pi TLE94112 Example
@@ -13,9 +7,9 @@
 
 #if (TLE94112_FRAMEWORK == TLE94112_FRMWK_RPI)
 
-Tle94112Rpi controller = Tle94112Rpi();
+//Tle94112Rpi controller = Tle94112Rpi();
 
-Tle94112Motor motor(controller);
+//Tle94112Motor motor(controller);
 
 int main() {
 	return 0;
---
+++ src/framework/raspberrypi/examples/basicTest.hpp
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "../../../config/tle94112-conf.hpp"
+#include <TLE94112-platf-rpi.hpp>
+#include <TLE94112-pal-rpi.hpp>
+#include <TLE94112.hpp>
+#include <TLE94112Motor.hpp>
