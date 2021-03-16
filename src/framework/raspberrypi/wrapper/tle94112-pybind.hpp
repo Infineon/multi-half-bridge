c98627b7d6f6f151bd28e468ffd788e08b62a5ab
Jaumann  Dominik  (IFAG DES SDF SCS EPE) <Dominik.Jaumann@infineon.com>
2021-03-16 14:57:22 +0100

    : Extended the already existing pybind11 wrappe, facing issues of ambiguity

--- src/framework/raspberrypi/wrapper/tle94112-pybind.hpp
+++ src/framework/raspberrypi/wrapper/tle94112-pybind.hpp
@@ -7,5 +7,9 @@
 
 #if (TLE94112_FRAMEWORK == TLE94112_FRMWK_RPI)
 
+#include "../../../corelib/tle94112.hpp"
+#include "../../../corelib/tle94112-motor.hpp"
+#include "tle94112-rpi.hpp"
+
 #endif /** TLE94112_FRAMEWORK **/
 #endif /** TLE94112_PYBIND_HPP_ **/
