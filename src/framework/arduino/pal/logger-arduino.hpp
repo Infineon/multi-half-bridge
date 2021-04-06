324b0dc8c0bc92cda686848038b0a6b53340d0e7
jaenrig-ifx <JuanAntonio.EnriquezGarcia@infineon.com>
2021-04-06 17:31:35 +0200

    Added logger core and Arduino PAL implementation

---
+++ src/framework/arduino/pal/logger-arduino.hpp
@@ -0,0 +1,47 @@
+/** 
+ * @file        logger-arduino.hpp
+ * @brief       TLE94112 Logger Arduino PAL 
+ * @date        April 2021
+ * @copyright   Copyright (c) 2020-2021 Infineon Technologies AG
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef LOGGER_ARDUINO_H_
+#define LOGGER_ARDUINO_H_
+
+#include "tle94112-conf.hpp"
+
+#if (TLE94112_FRAMEWORK == TLE94112_FRMWK_ARDUINO)
+
+#include "logger.hpp"
+
+#if (TLE94112_LOGGER_ENABLED == 1)
+
+using namespace tle94112;
+
+/**
+ * @addtogroup arduinoPal
+ * @{
+ */
+
+class LoggerIno : public LoggerPAL
+{
+        public:
+                        LoggerIno    ();
+                       ~LoggerIno    (); 
+            Error_t     init         ();
+            Error_t     deinit       ();
+            Error_t     write        (const uint8_t * log_data, uint32_t length);
+};
+
+/** @} */
+
+/**
+ * @brief Default LoggerIno
+ */
+extern LoggerIno logpalino;
+
+#endif /* TLE94112_LOGGER_ENABLED */
+#endif /** TLE94112_FRAMEWORK **/
+#endif /** LOGGER_ARDUINO_H_ **/
