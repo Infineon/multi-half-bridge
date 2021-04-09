d7f0447d6cb7df4466e9390c574c55b580a374cd
Julian Eder <Julian.Eder@infineon.com>
2021-04-09 07:29:52 +0200

    add stepper motor example

--- src/framework/raspberrypi/examples/stepperMotor.cpp
+++ src/framework/raspberrypi/examples/stepperMotor.cpp
@@ -28,15 +28,68 @@
 /* 3rd party libraries for this example */
 #include <bcm2835.h> // This library is required for the delay() function.
 
+void step(uint32_t steps) {
+  for (uint32_t step = 0; step < steps; step++) {
+
+  }
+}
+
+class BipolarStepper {
+  public:
+    enum direction
+		{
+			DIR_FORWARD = 0,
+			DIR_BACKWARD
+		};
+    Tle94112 *mDriver;
+    Tle94112::HalfBridge hb_a1, hb_a2, hb_b1, hb_b2;
+    int8_t state = 0;
+    BipolarStepper(Tle94112 &mDriver, Tle94112::HalfBridge a1, Tle94112::HalfBridge a2, Tle94112::HalfBridge b1, Tle94112::HalfBridge b2) {
+      this->mDriver = &mDriver;
+      hb_a1 = a1;
+      hb_a2 = a2;
+      hb_b1 = b1;
+      hb_b2 = b2;
+    }
+    void disableOutputs() {
+      mDriver->configHB(hb_a1, mDriver->TLE_FLOATING, mDriver->TLE_NOPWM);
+      mDriver->configHB(hb_a2, mDriver->TLE_FLOATING, mDriver->TLE_NOPWM);
+      mDriver->configHB(hb_b1, mDriver->TLE_FLOATING, mDriver->TLE_NOPWM);
+      mDriver->configHB(hb_b2, mDriver->TLE_FLOATING, mDriver->TLE_NOPWM);
+    }
+    void fullStep(BipolarStepper::direction dir) {
+      switch(state) {
+        case 0:
+          mDriver->configHB(hb_a1, mDriver->TLE_HIGH, mDriver->TLE_NOPWM);
+          mDriver->configHB(hb_a2, mDriver->TLE_LOW, mDriver->TLE_NOPWM);
+          break;
+        case 1:
+          mDriver->configHB(hb_b1, mDriver->TLE_HIGH, mDriver->TLE_NOPWM);
+          mDriver->configHB(hb_b2, mDriver->TLE_LOW, mDriver->TLE_NOPWM);
+          break;
+        case 2:
+          mDriver->configHB(hb_a2, mDriver->TLE_HIGH, mDriver->TLE_NOPWM);
+          mDriver->configHB(hb_a1, mDriver->TLE_LOW, mDriver->TLE_NOPWM);
+          break;
+        case 3:
+          mDriver->configHB(hb_b2, mDriver->TLE_HIGH, mDriver->TLE_NOPWM);
+          mDriver->configHB(hb_b1, mDriver->TLE_LOW, mDriver->TLE_NOPWM);
+          break;
+      }
+      if (dir == DIR_FORWARD) state++;
+      if (dir == DIR_BACKWARD) state--;
+      if (state < 0) state = 3;
+      if (state > 3) state = 0;
+    }
+};
+
+// Set 200 steps per motor revision (usual for many stepper motors).
+uint16_t steps_per_rev = 200;
+
 int main(int argc, char const *argv[])
 {
-  
   // Create a Tle94112Rpi instance for each motor controller.
   Tle94112Rpi controller;
-
-  // Create a Tle94112Motor instance for each connected load.
-  Tle94112Motor a(controller);
-  Tle94112Motor b(controller);
 
   // Enable MotorController.
   // Note: Required to be done before starting to configure the motor
@@ -46,25 +99,20 @@ int main(int argc, char const *argv[])
   // Clear all errors to start clean
   controller.clearErrors();
 
-  // Let the library know that the stepper motor is connected to:
-  // OUT1: A1
-  // OUT5: A2
-  // OUT7: B1
-  // OUT9: B2
-  motor.connect(motor.HIGHSIDE, controller.TLE_HB1);
-  motor.connect(motor.LOWSIDE, controller.TLE_HB5);
-  motor.setPwm(motor.LOWSIDE, controller.TLE_NOPWM);
-  
-  // Initialize the motor.
-  motor.begin();
+  // Create object from BipolarStepper class to control bipolar stepper motors.
+  // First argument is the motor controller, following arguments are coil A high side,
+  // coil A low side, coil B high side, coil B low side.
+  BipolarStepper stepper(controller, controller.TLE_HB1, controller.TLE_HB5, controller.TLE_HB7, controller.TLE_HB9);
 
-  // Switch the load on.
-  motor.start(255);
+  // Turn the motor for one revision.
+  for (uint16_t i = 0; i < steps_per_rev; i++) {
+    stepper.fullStep(stepper.DIR_FORWARD);
+    delay(1000/steps_per_rev); // Take 1000ms for one revision.
+  }
 
-  delay(3000);
-
-  // Switch the load off (outputs to floating state).
-  motor.coast();
+  // Disable the outputs. Will release any forces from the motor.
+  // REQUIRED, otherwise the motor becomes hot.
+  stepper.disableOutputs();
 }
 
 #endif /** TLE94112_FRAMEWORK **/
