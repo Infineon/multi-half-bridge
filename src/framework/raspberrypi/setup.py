7ff11c3accc0760f4f3c8c0b9a22093247db43a3
EricSchulze <ericjoerg.schulze@infineon.com>
2024-09-09 16:51:08 +0200

    ich weiß, nicht, warum ich das gerade nicht installieren kann

--- src/framework/raspberrypi/setup.py
+++ src/framework/raspberrypi/setup.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-
 import os
 import sys
 import subprocess
-
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
@@ -15,9 +13,6 @@ PLAT_TO_CMAKE = {
     "win-arm64": "ARM64",
 }
 
-# A CMakeExtension needs a sourcedir instead of a file list.
-# The name must be the _single_ output extension from the CMake build.
-# If you need multiple extensions, see scikit-build.
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=""):
         Extension.__init__(self, name, sources=[])
@@ -27,81 +22,57 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
 
-        # required for auto-detection of auxiliary "native" libs
         if not extdir.endswith(os.path.sep):
             extdir += os.path.sep
 
         cfg = "Debug" if self.debug else "Release"
 
-        # CMake lets you override the generator - we need to check this.
-        # Can be set with Conda-Build, for example.
         cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
 
-        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
-        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
-        # from Python.
+        # Ensure necessary environment variables are set
+        cmake_prefix_path = subprocess.check_output(
+            [sys.executable, "-m", "pybind11", "--cmakedir"]
+        ).strip().decode()
+        os.environ["CMAKE_PREFIX_PATH"] = cmake_prefix_path
+
         cmake_args = [
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
             "-DPYTHON_EXECUTABLE={}".format(sys.executable),
             "-DEXAMPLE_VERSION_INFO={}".format(self.distribution.get_version()),
-            "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
+            "-DCMAKE_BUILD_TYPE={}".format(cfg),
         ]
+
         build_args = []
 
         if self.compiler.compiler_type != "msvc":
-            # Using Ninja-build since it a) is available as a wheel and b)
-            # multithreads automatically. MSVC would require all variables be
-            # exported for Ninja to pick it up, which is a little tricky to do.
-            # Users can override the generator with CMAKE_GENERATOR in CMake
-            # 3.15+.
             if not cmake_generator:
                 cmake_args += ["-GNinja"]
-
         else:
-            # Single config generators are handled "normally"
             single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
-
-            # CMake allows an arch-in-generator style for backward compatibility
             contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
 
-            # Specify the arch if using MSVC generator, but only if it doesn't
-            # contain a backward-compatibility arch spec already in the
-            # generator name.
             if not single_config and not contains_arch:
                 cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
 
-            # Multi-config generators have a different way to specify configs
             if not single_config:
                 cmake_args += [
                     "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
                 ]
                 build_args += ["--config", cfg]
 
-        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
-        # across all generators.
         if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            # self.parallel is a Python 3 only way to set parallel jobs by hand
-            # using -j in the build_ext call, not supported by pip or PyPA-build.
             if hasattr(self, "parallel") and self.parallel:
-                # CMake 3.12+ only.
                 build_args += ["-j{}".format(self.parallel)]
 
-        if os.path.exists(self.build_temp):
-            # Remove the build directory to ensure a clean build
-            subprocess.check_call(["rm", "-rf", self.build_temp])
-        
-        os.makedirs(self.build_temp, exist_ok=True)
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
 
-        sourcedir = os.path.abspath(os.path.join(ext.sourcedir, '../..'))
+        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp)
         subprocess.check_call(
-            ["cmake", sourcedir] + cmake_args, cwd=self.build_temp
-        )
-        subprocess.check_call(
-            ["cmake", "--build", ".", "--target", "multi-half-bridge-corelib"] + build_args, cwd=self.build_temp
+            ["cmake", "--build", "."] + build_args,
+            cwd=self.build_temp
         )
 
-# The information here can also be placed in setup.cfg - better separation of
-# logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="multi-half-bridge",
     version="5.0.1",
@@ -113,7 +84,7 @@ setup(
         'Wiki': 'https://github.com/Infineon/multi-half-bridge/wiki',
         'IC Products Page': 'https://www.infineon.com/cms/de/product/power/motor-control-ics/brushed-dc-motor-driver-ics/multi-half-bridge-ics/'
     },
-    ext_modules=[CMakeExtension("multi_half_bridge_py")],
+  #  ext_modules=[CMakeExtension("multi_half_bridge_py", sourcedir=".")],
     cmdclass={"build_ext": CMakeBuild},
     license='MIT',
     url='https://pypi.org/project/multi-half-bridge/',
@@ -124,4 +95,3 @@ setup(
     ],
     zip_safe=False,
 )
-#check :)
