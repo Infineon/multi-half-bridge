7964f590f85709af0aa0f50e215eb7b8213098f6
EricSchulze <EricJoerg.Schulze@infineon.com>
2024-09-10 15:08:09 +0200

    added removed comments in setup.py

--- src/framework/raspberrypi/setup.py
+++ src/framework/raspberrypi/setup.py
@@ -13,6 +13,10 @@ PLAT_TO_CMAKE = {
     "win-arm64": "ARM64",
 }
 
+# A CMakeExtension needs a sourcedir instead of a file list.
+# The name must be the _single_ output extension from the CMake build.
+# If you need multiple extensions, see scikit-build.
+
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=""):
         Extension.__init__(self, name, sources=[])
@@ -22,11 +26,14 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
 
+         # required for auto-detection of auxiliary "native" libs
         if not extdir.endswith(os.path.sep):
             extdir += os.path.sep
 
         cfg = "Debug" if self.debug else "Release"
 
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
         cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
 
         # Ensure necessary environment variables are set
@@ -45,23 +52,40 @@ class CMakeBuild(build_ext):
         build_args = []
 
         if self.compiler.compiler_type != "msvc":
+             # Using Ninja-build since it a) is available as a wheel and b)
+            # multithreads automatically. MSVC would require all variables be
+            # exported for Ninja to pick it up, which is a little tricky to do.
+            # Users can override the generator with CMAKE_GENERATOR in CMake
+            # 3.15+.
             if not cmake_generator:
                 cmake_args += ["-GNinja"]
         else:
+             # Single config generators are handled "normally"
             single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
             contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
 
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
             if not single_config and not contains_arch:
                 cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
 
+            # Multi-config generators have a different way to specify configs
             if not single_config:
                 cmake_args += [
                     "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
                 ]
                 build_args += ["--config", cfg]
 
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+        # across all generators.
         if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # self.parallel is a Python 3 only way to set parallel jobs by hand
+            # using -j in the build_ext call, not supported by pip or PyPA-build.
             if hasattr(self, "parallel") and self.parallel:
+                 # CMake 3.12+ only.
                 build_args += ["-j{}".format(self.parallel)]
 
         if not os.path.exists(self.build_temp):
@@ -72,7 +96,8 @@ class CMakeBuild(build_ext):
             ["cmake", "--build", "."] + build_args,
             cwd=self.build_temp
         )
-
+# The information here can also be placed in setup.cfg - better separation of
+# logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="multi-half-bridge",
     version="5.0.1",
